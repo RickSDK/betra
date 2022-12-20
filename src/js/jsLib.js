1e2318630d068d4a7e4cf9dfaac2d18128ac680e
Rick Medved <rickmedved@Ricks-iMac.local>
2022-12-19 21:05:45 -0800

    trying to make it better

--- src/js/jsLib.js
+++ src/js/jsLib.js
@@ -2,7 +2,7 @@ function getHostname() {
     return 'https://www.appdigity.com/betraPhp/';
 }
 function getVersion() {
-	return '0.80x1';
+	return '0.80x2';
 }
 function getIPInfo(userName, pwd) {
     var code = btoa(pwd);
